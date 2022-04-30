65b1160783c6556dabcac14a29cd2bf89ad21e2a
PatrykRomanowski <71602577+PatrykRomanowski@users.noreply.github.com>
2022-04-30 20:09:33 +0200

    adding loading file

--- task.cpp
+++ task.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 
 
-main()
+main(int argc, char * argv[])
 {
   string helloworld = "hello w3orld";
 
    cout << helloworld << endl;
+
+   std::cout << "argc =  : " << argc << std::endl; 
+    int iter = 0;
+    for (iter = 0; iter < argc; iter++) {
+        std::cout << "argv[" << iter << "] =" << argv[iter] << std::endl;
+    }
 
 }
