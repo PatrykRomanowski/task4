00a17afada0f4cd3d45b4cb0b96f83069d5342f8
PatrykRomanowski <71602577+PatrykRomanowski@users.noreply.github.com>
2022-05-01 13:47:10 +0200

    add logger

--- task.cpp
+++ task.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <fstream>
+#include <ctime>
+
+
 using namespace std;
+
+std::fstream logFile; //handler for log file
+
+
+void saveLog(std::string msg)
+{
+    std::string ss;
+    time_t currentTime = time(NULL);
+    std::string txttime = (std::string)asctime(localtime(&currentTime));
+    txttime = txttime.substr(0, txttime.length() - 1);
+    ss = (std::string)"T: " + txttime + " M: " + msg + "\n";
+    logFile << ss.c_str();
+    logFile.flush();
+    std::cout << ss.c_str();
+    std::cout.flush();
+}
+
+void openLog(std::string fileName)
+{
+    logFile.open(fileName.c_str(), std::ios_base::app);
+    if (!logFile.good() == true)
+    {
+        std::cout << "Cannot open log file " << "log.txt" << std::endl;
+    }
+    saveLog("Log file opened");
+}
+
+void closeLog(void)
+{
+    saveLog("Log file closed");
+    logFile.close();
+}
+
 
 
 main(int argc, char * argv[])
 {
+    std::cout << "BER Calc v1.0" << std::endl;
+    openLog("log.log");
+    saveLog("Program uruchomiono poprawnie");
+    closeLog();
+
+
   string helloworld = "hello w3orld";
 
    cout << helloworld << endl;
